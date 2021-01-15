990a7702c9fa2f8c21d96fee58eff203f694bf5c
John <jddcp@ic.ufal.br>
2021-01-15 01:31:23 -0300

    Adding first command implementation (DC and UPPER)

--- Client/ClientMultiThread.py
+++ Client/ClientMultiThread.py
@@ -9,11 +9,27 @@ print("\n-------------------------\n")
 # Setting the address that the client will connect
 HOST_AND_PORT = ("localhost", 21000)
 
+def serverResponses(serverResponse):
+    print("\n-------------------------\n")
+    print("Server returned as response:", serverResponse.decode() + '\n')
+    return
 # Creating the socket for the client with its types of connection
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(HOST_AND_PORT) # Connecting to the tuple with the server's address
-textToSend = input("Type the text you want to send: ")
-clientSocket.sendall(str.encode(textToSend))
-serverResponse = clientSocket.recv(4096)
-print("\n-------------------------\n")
-print("Server returned as response:", serverResponse.decode())
+while True:
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect(HOST_AND_PORT) # Connecting to the tuple with the server's address
+
+    textToSend = input("Type the command: ")
+    clientSocket.sendall(str.encode(textToSend))
+    serverResponse = clientSocket.recv(4096)
+    serverResponses(serverResponse)
+
+    if serverResponse.decode() == '100 # SHUTTING DOWN THE CONNECTION': # make an array of possible commands just to simplify
+        print("\n-------------------------\n")
+        break
+
+    contentToSend = input("Type the text you want to send: ")
+    clientSocket.sendall(str.encode(contentToSend))
+    serverResponse2 = clientSocket.recv(2048)
+    serverResponses(serverResponse2)
+
+    
--- Server/ServerMultiThread.py
+++ Server/ServerMultiThread.py
@@ -7,6 +7,9 @@ print("\nSTARTING MULTITHREAD SERVER\n")
 print("Welcome to John's test server, you are in the server side")
 print("\n-------------------------\n")
 
+#Creating the command's array and command's content
+commands = ['DISCONNECT', 'UPPER']
+commandContent = ['UPPER#']
 # Setting the socket's address
 HOST_AND_PORT = ("localhost", 21000)
 
@@ -15,24 +18,25 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connecting the socket's address to the socket's instance
 serverSocket.bind(HOST_AND_PORT)
-serverSocket.listen(6) # Create the number of connections accepted by the server
-print("[*] Listen to " + HOST_AND_PORT[0] + ":" + str(HOST_AND_PORT[1]))
+serverSocket.listen(15) # Create the number of connections accepted by the server
+print("[*] Listen to " + HOST_AND_PORT[0] + ":" + str(HOST_AND_PORT[1]))  
+print("\n-------------------------\n")
 
 def clientHandler(clientSocket):
     dataReceived = clientSocket.recv(1024) # Stabilishes the limit size of the data received from client
     print("[*] Receiving: ", dataReceived.decode())
     print("\n-------------------------\n")
-    clientSocket.sendall(dataReceived.upper())
-    clientSocket.close()
-
+    if dataReceived.decode() == commands[0]:
+        clientSocket.sendall(str.encode("100 # SHUTTING DOWN THE CONNECTION"))
+        clientSocket.close()
+        return
+    elif dataReceived.decode() == commands[1]:
+        clientSocket.sendall(str.encode("200 # CONNECTION STABILISHED"))
+        c = clientSocket.recv(1024)
+        clientSocket.sendall(c.upper())
+    
 while True:
     conn, addr = serverSocket.accept() # Store the connection and the address of the client who is connected
     print("[*] Connection accepted by: " + addr[0] + ":" + str(addr[1]))
     threadHandler = threading.Thread(target=clientHandler, args=(conn, ))
     threadHandler.start()
-    
-
-
-
-
-
