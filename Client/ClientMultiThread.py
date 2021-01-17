755edd196b9a698004c3beae34d8937cf6883a87
John <jddcp@ic.ufal.br>
2021-01-17 00:53:07 -0300

    Fixed the client connection problem

--- Client/ClientMultiThread.py
+++ Client/ClientMultiThread.py
@@ -3,33 +3,35 @@ import socket
 # Multi thread client using TCP 
 
 print("\nSTARTING MULTITHREAD CLIENT\n")
-print("Welcome to John's test client, you are in the client side")
+print("Welcome to John and Hiago's test client, you are in the client side")
 print("\n-------------------------\n")
 
+commandsDictionary = ["100 # SHUTTING DOWN THE CONNECTION"]
 # Setting the address that the client will connect
-HOST_AND_PORT = ("localhost", 21000)
+HOST_AND_PORT = ("localhost", 22000)
+
+# Creating the socket for the client with its types of connection
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect(HOST_AND_PORT) # Connecting to the tuple with the server's address
 
 def serverResponses(serverResponse):
     print("\n-------------------------\n")
     print("Server returned as response:", serverResponse.decode() + '\n')
     return
-# Creating the socket for the client with its types of connection
-while True:
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect(HOST_AND_PORT) # Connecting to the tuple with the server's address
 
+while True:
     textToSend = input("Type the command: ")
     clientSocket.sendall(str.encode(textToSend))
     serverResponse = clientSocket.recv(4096)
     serverResponses(serverResponse)
 
-    if serverResponse.decode() == '100 # SHUTTING DOWN THE CONNECTION': # make an array of possible commands just to simplify
-        print("\n-------------------------\n")
+    if serverResponse.decode() == commandsDictionary[0]: # make an array of possible commands just to simplify
+        print("-------------------------\n")
         break
 
     contentToSend = input("Type the text you want to send: ")
     clientSocket.sendall(str.encode(contentToSend))
-    serverResponse2 = clientSocket.recv(2048)
-    serverResponses(serverResponse2)
+    serverResponse = clientSocket.recv(4096)
+    serverResponses(serverResponse)
 
     
--- Server/ServerMultiThread.py
+++ Server/ServerMultiThread.py
@@ -4,14 +4,14 @@ import threading
 # Multi thread server using TCP 
 
 print("\nSTARTING MULTITHREAD SERVER\n")
-print("Welcome to John's test server, you are in the server side")
+print("Welcome to John and Hiago's test server, you are in the server side")
 print("\n-------------------------\n")
 
 #Creating the command's array and command's content
 commands = ['DISCONNECT', 'UPPER']
-commandContent = ['UPPER#']
+
 # Setting the socket's address
-HOST_AND_PORT = ("localhost", 21000)
+HOST_AND_PORT = ("localhost", 22000)
 
 # Creating the socket for the server
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,17 +23,21 @@ print("[*] Listen to " + HOST_AND_PORT[0] + ":" + str(HOST_AND_PORT[1]))
 print("\n-------------------------\n")
 
 def clientHandler(clientSocket):
-    dataReceived = clientSocket.recv(1024) # Stabilishes the limit size of the data received from client
-    print("[*] Receiving: ", dataReceived.decode())
-    print("\n-------------------------\n")
-    if dataReceived.decode() == commands[0]:
-        clientSocket.sendall(str.encode("100 # SHUTTING DOWN THE CONNECTION"))
-        clientSocket.close()
-        return
-    elif dataReceived.decode() == commands[1]:
-        clientSocket.sendall(str.encode("200 # CONNECTION STABILISHED"))
-        c = clientSocket.recv(1024)
-        clientSocket.sendall(c.upper())
+    while True:
+        dataReceived = clientSocket.recv(2048) # Estabilishes the limit size of the data received from client
+        print("[*] Receiving: ", dataReceived.decode())
+        print("\n-------------------------\n")
+        # a(dataReceived, clientSocket)
+
+        if dataReceived.decode() == commands[0]:
+            clientSocket.send(str.encode("100 # SHUTTING DOWN THE CONNECTION"))
+            clientSocket.close()
+            break
+        elif dataReceived.decode() == commands[1]:
+            clientSocket.send(str.encode("150 # COMMAND ACKNOWLEDGED"))
+            dataReceived = clientSocket.recv(1024)
+            clientSocket.send(dataReceived.upper())
+    return
     
 while True:
     conn, addr = serverSocket.accept() # Store the connection and the address of the client who is connected
